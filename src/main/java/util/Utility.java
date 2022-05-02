7f18225de69ea8e321f9921d4a30b57628bdd6bb
oshirotakahiro <22oshiro_takahiro@axiz.co.jp>
2022-05-02 14:53:22 +0900

    基本-訂正(石の改行)

--- src/main/java/util/Utility.java
+++ src/main/java/util/Utility.java
@@ -10,18 +10,13 @@ public class Utility {
         }
     }
 
-    /**
-     * 石の残数に応じて表示する"●"用のhtmlソースを生成します
-     * @return
-     */
-    // todo:ここにgetStoneDisplayHtmlメソッドを定義
     public static String getStoneDisplayHtml(int n) {
     	String stone = "";
+    	int count = 1;
     	for (int i = 0; i < n; i++) {
-    		int count = 1;
     		stone += "●";
     		if (count % 10 == 0) {
-    			stone = "<br>";
+    			stone += "<br>";
     		}
     		count++;
     	}
